1c3abc28a1cee2b8333af489f50173f6733b680d
nawapat-urnique <nawapat.m@urniquestudio.com>
2023-03-05 07:05:01 +0700

    add code day1

---
+++ src/main/kotlin/day1.kt
@@ -0,0 +1,20 @@
+
+const val inputFile = "day1.txt"
+
+fun scanDeeper(inputs: List<Int>): Int {
+    return inputs.windowed(2).count { (a, b) -> a < b }
+}
+
+fun sumThree(inputs: List<Int>): List<Int> {
+    return inputs.windowed(3).map { (a, b, c) -> a + b + c }
+}
+
+fun main(args: Array<String>) {
+    val data = {}::class.java.getResource(inputFile).readText()
+    val intArr = data.lines().map { it.toInt() }
+    val countDeeper = scanDeeper(intArr)
+    val countSumThreeDeeper = scanDeeper(sumThree(intArr))
+
+    println(countDeeper)
+    println(countSumThreeDeeper)
+}
